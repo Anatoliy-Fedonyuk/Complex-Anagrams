8d6bbe0aedac5b46b6ba1e1576333a4bb0725546
Anatoliy 1974 <126827447+Anatoliy-Fedonyuk@users.noreply.github.com>
2023-10-25 12:43:42 +0200

    ver 1.0.0

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -3,6 +3,10 @@ from functools import lru_cache
 from loguru import logger
 
 
+logger.add('debug.log', format='{time} {level} {message}', level='DEBUG')
+logger.info(f"[INFO] Start working application {__name__} !")
+
+
 @lru_cache(maxsize=1024)
 def get_anagram(text: str) -> str:
     """
@@ -11,22 +15,29 @@ def get_anagram(text: str) -> str:
     :param text: string input
     :return: string
     """
-    if not isinstance(text, str):
-        raise TypeError('--Only a string can be used as input!--')
-    if text == "":
-        raise ValueError('--Anagram do not make sense, string empty--')
-    words = text.split()
-    if all(w.isdigit() for w in words):
-        raise ValueError('--Anagram do not make sense in your string only digits--')
-    reversed_w = []
-    for word in words:
-        no_letter = [(i, ch) for i, ch in enumerate(word) if not ch.isalpha()]
-        word = "".join(ch for ch in word if ch.isalpha())
-        revers = word[::-1]
-        for i, ch in no_letter:
-            revers = f'{revers[:i]}{ch}{revers[i:]}'
-        reversed_w.append(revers)
-    return ' '.join(reversed_w)
+    try:
+        if not isinstance(text, str):
+            logger.error("[ERROR] TypeError - only a string is expected.")
+            raise TypeError('--Only a string can be used as input!--')
+        if text == "":
+            logger.error("[ERROR] ValueError - a string is empty.")
+            raise ValueError('--Anagram do not make sense, string is empty--')
+        words = text.split()
+        if all(w.isdigit() for w in words):
+            logger.error("[ERROR] ValueError - a string is empty.")
+            raise ValueError('--Anagram do not make sense in your string only digits--')
+        reversed_w = []
+        for word in words:
+            no_letter = [(i, ch) for i, ch in enumerate(word) if not ch.isalpha()]
+            word = "".join(ch for ch in word if ch.isalpha())
+            revers = word[::-1]
+            for i, ch in no_letter:
+                revers = f'{revers[:i]}{ch}{revers[i:]}'
+            reversed_w.append(revers)
+        return ' '.join(reversed_w)
+    except Exception as e:
+        logger.error(f"[ERROR] Unexpected error occurred: {e}")
+        raise # Re-raise the exception for further handling
 
 
 if __name__ == '__main__':
